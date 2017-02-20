b8d6b1c3f1f5f1f191c156de191bd5ccbdb99d9e
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2017-02-20 15:54:55 +0100

    Updates the setup fileº

--- setup.py
+++ setup.py
@@ -1,35 +1,12 @@
-#!/usr/bin/env python
-
-from setuptools import setup
-
-# auto-convert README.md
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (ImportError, OSError):
-    # we'll just use the poorly formatted Markdown file instead
-    long_description = open('README.md').read()
+from distutils.core import setup
 
 setup(
     name='goproApi',
     version='0.0.1',
-    description='A python3 API for controlling a GoPro.',
-    long_description=long_description,
+    packages=['goproApi'],
     url='https://github.com/nacho-carnicero/goproApi',
-    author='Nacho Carnicero',
+    license='LICENSE',
+    author='Ignacio Carnicero',
     author_email='ignacio.carnicero@sterblue.com',
-    license=open('LICENSE').read(),
-    packages=['src'],
-    setup_requires=[
-        'tox',
-        'nose',
-        'flake8'
-    ],
-    install_requires=[
-        'Pillow',
-        'wireless',
-        'colorama',
-        'goprohero'
-    ],
-    zip_safe=False
+    description='Python3 API for Gopro cameras',
 )
